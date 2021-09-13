3be639c08e8c7b6b27741945fa97455627abb674
B2B\bureap <bureap@b2b.regn.net>
2021-09-13 12:05:13 +0100

    exrerewr

---
+++ Skill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Skill
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start(DateTime.Now);
+
+                for (var j = 0; j <= 100; j++)
+                {
+                    Thread.Sleep(12);
+                }
+
+                stopwatch.Stop(DateTime.Now);
+
+                Console.WriteLine("Time spend:" + stopwatch.GetInterval().ToString());
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ Skill/Stopwatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Skill
+{
+    public class Stopwatch
+    { 
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        private bool running = false;
+
+        public void Start(DateTime start)
+        {
+            if (!running)
+            {
+                StartTime = start;
+                running = true;
+            }
+            else
+            {
+                throw new InvalidExpressionException("Stopwatch is already running!!!");
+            }
+        }
+
+        public void Stop(DateTime stop)
+        {
+            if (running)
+            {
+                EndTime = stop;
+                running = false;
+            }
+        }
+
+        public TimeSpan GetInterval()
+        {
+            var duration = EndTime - StartTime;
+            return duration;
+        }
+    }
+}
